4a88d1d1e034fd953b272112fd0312a021c3102e
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-10-28 16:58:46 +0200

    Add project files.

---
+++ Kappitel 4 uppgift 4.31/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en mening");
+            string mening = Console.ReadLine();
+            bool ord =false;
+
+            for (int i = 0; i < mening.Length; i++)
+            {
+                if (mening[..i].Contains(mening[i]))
+                {
+                    continue;
+                }
+                if (mening[(i + 1)..].Contains(mening[i]))
+                {
+                    Console.WriteLine($"texten innerhåller {mening[i]} mer än en gång");
+                    ord = true;
+                }
+
+            }
+            if (ord == false)
+            {
+                Console.WriteLine("Ingen bokstav förekommer mer än en gång");
+            }
+        }
+    }
+}
